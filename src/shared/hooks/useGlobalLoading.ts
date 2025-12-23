f6d467b2e7cdee6956e6ee97b54ed3bf88d1e910
Oleg Kova <okovalchuk722@gmail.com>
2025-12-24 01:01:15 +0300

    added global loader fix

--- src/shared/hooks/useGlobalLoading.ts
+++ src/shared/hooks/useGlobalLoading.ts
@@ -1,8 +1,5 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "@/app/providers/store";
-import { movieApi } from "@/features/main/api/movieApi";
-
-const excludedEndpoints = [movieApi.endpoints.getCategoryMovies.name];
 
 export const useQueryGlobalLoading = () => {
   return useSelector((state: RootState) => {
@@ -11,13 +8,9 @@ export const useQueryGlobalLoading = () => {
     return queries.some((query) => {
       if (query?.status !== "pending") {
         return;
-      } else if (
-        excludedEndpoints.includes(query.endpointName) ||
-        query.endpointName !== movieApi.endpoints.getCategoryMovies.name
-      ) {
-        const completed = queries.filter((q) => q?.status === "fulfilled");
-        return completed.length > 0;
       }
+      const completed = queries.filter((q) => q?.status === "fulfilled");
+      return completed.length > 0;
     });
   });
 };
