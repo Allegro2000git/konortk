f629f19fa7957149dd7c9182532c04a866b903d2
Oleg Kova <okovalchuk722@gmail.com>
2025-12-24 03:25:48 +0300

    testing skeleton for main

--- src/features/main/ui/Main.tsx
+++ src/features/main/ui/Main.tsx
@@ -2,12 +2,14 @@ import { SearchInput } from "@/features/searchInput/SearchInput";
 import { MovieSection } from "@/features/movieSections/MovieSection";
 import s from "./Main.module.css";
 import { useCategoryMovies, useMovieBackdropPath } from "@/shared/hooks";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export function Main() {
-  const { data: popularData, isLoading } = useCategoryMovies("popular", { page: 1 });
-  const { data: topRatedData } = useCategoryMovies("top_rated", { page: 1 });
-  const { data: nowPlayingData } = useCategoryMovies("upcoming", { page: 1 });
-  const { data: upcomingData } = useCategoryMovies("now_playing", { page: 1 });
+  const { data: popularData, isLoading: popularLoading } = useCategoryMovies("popular", { page: 1 });
+  const { data: topRatedData, isLoading: topRatedLoading } = useCategoryMovies("top_rated", { page: 1 });
+  const { data: nowPlayingData, isLoading: nowPlayingLoading } = useCategoryMovies("upcoming", { page: 1 });
+  const { data: upcomingData, isLoading: upcomingLoading } = useCategoryMovies("now_playing", { page: 1 });
   const movieBackdropPath = useMovieBackdropPath(popularData);
 
   const sections = [
@@ -33,9 +35,7 @@ export function Main() {
     },
   ];
 
-  if (isLoading) {
-    return <h2>Skeleton loader...</h2>;
-  }
+  const isSectionLoading = popularLoading || topRatedLoading || nowPlayingLoading || upcomingLoading;
 
   return (
     <>
@@ -48,15 +48,46 @@ export function Main() {
         }}
       >
         <div className={s["cover-container"]}>
-          <h1>Welcome</h1>
-          <h3 className={s.subtitle}>Browse highlighted titles from TMDB</h3>
-          <SearchInput isSearchButtonActive={true} />
+          {isSectionLoading ? (
+            <>
+              <Skeleton width={200} height={40} />
+              <Skeleton width={300} height={24} style={{ margin: "1rem 0" }} />
+              <Skeleton width={500} height={40} />
+            </>
+          ) : (
+            <>
+              <h1>Welcome</h1>
+              <h3 className={s.subtitle}>Browse highlighted titles from TMDB</h3>
+              <SearchInput isSearchButtonActive={true} />
+            </>
+          )}
         </div>
       </div>
       <section className={s["cover-container"]}>
-        {sections.map((section, index) => (
-          <MovieSection key={index} title={section.title} movies={section.movies} category={section.category} />
-        ))}
+        {isSectionLoading
+          ? Array.from({ length: 4 }).map((_, index) => (
+              <div key={`skeleton-${index}`}>
+                <Skeleton width={200} height={28} style={{ margin: "2rem 0" }} />
+
+                <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(auto-fill, minmax(150px, 1fr))",
+                    gap: "1rem",
+                  }}
+                >
+                  {Array.from({ length: 6 }).map((_, index) => (
+                    <div key={`movie-${index}`} style={{ marginBottom: "2rem" }}>
+                      <Skeleton height={250} style={{ marginBottom: "1rem" }} />
+                      <Skeleton width={125} height={20} />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))
+          : sections.map((section, index) => (
+              <MovieSection key={index} title={section.title} movies={section.movies} category={section.category} />
+            ))}
       </section>
     </>
   );
